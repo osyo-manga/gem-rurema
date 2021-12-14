9cb75d71036b982b17f6ef1a6594b768a3ebd2eb
manga_osyo <manga.osyo@gmail.com>
2021-12-14 23:44:10 +0900

    Delete debug output.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,23 +2,12 @@ require "rubygems"
 
 
 Gem.pre_install do |installer|
-  pp "==== rubygems_plugin rurema ===="
-  pp Dir.getwd
-
   versions = %w(2_6_0 2_7_0 3_0_0 3_1_0).map { |v| v.gsub("_", ".") }
   target = RUBY_VERSION
 
   current_ver = versions.map { |v| Gem::Version.create(v) }.select { |v| v <= Gem::Version.create(target) }.sort.last.to_s
-  pp current_ver
   exclude = versions - [current_ver]
   full_gem_path = installer.spec.full_gem_path
-  files = Dir[*exclude.map { |ver| p File.join(full_gem_path, "lib/rurema/docs", ver.gsub(".", "_"), "/**") }]
-  pp files
+  files = Dir[*exclude.map { |ver| File.join(full_gem_path, "lib/rurema/docs", ver.gsub(".", "_"), "/**") }]
   files.map { |file| File.delete(file) }
-
-#   result = versions.map { |v| Gem::Version.create(v) }.select { |v| v <= Gem::Version.create(target) }.sort.last
-#   installer.spec.files = Dir['README.md', 'lib/*', "lib/rurema/docs/#{result.to_s.gsub(".", "_")}/**", base: installer.spec.full_gem_path]
-#   pp installer
-#   pp installer.spec.full_gem_path
-#   pp installer.spec.files
 end
